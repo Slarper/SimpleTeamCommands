cca2d4689bc08c322cbe1d59e57703825cc64df7
Slarper <naivesimple@outlook.com>
2022-11-05 09:12:51 +0800

    Initial commit

---
+++ src/main/java/slarper/simptc/command/To.java
@@ -0,0 +1,24 @@
+package slarper.simptc.command;
+
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+
+public class To extends CommandBase {
+    @Override
+    public String getName() {
+        return "to";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return null;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+
+    }
+}
