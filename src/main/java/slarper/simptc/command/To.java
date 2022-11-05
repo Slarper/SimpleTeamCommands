17f1ec2b772ab7aea0f1234d8a1089dafacb214b
Slarper <naivesimple@outlook.com>
2022-11-05 10:49:00 +0800

    add to command.

--- src/main/java/slarper/simptc/command/To.java
+++ src/main/java/slarper/simptc/command/To.java
@@ -1,10 +1,20 @@
 package slarper.simptc.command;
 
-import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import slarper.simptc.util.SimpleTeamCommandsUtils;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+/*
+ * to : teleport to the nearest player
+ * to [playerName] : teleport to the player
+ */
 
 public class To extends CommandBase {
     @Override
@@ -19,6 +29,36 @@ public class To extends CommandBase {
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        String playerName = null;
+        if (args.length == 0) {
+            Optional<String> playerName1 = Arrays.stream(server.getOnlinePlayerNames())
+                    .filter(name -> !name.equals(sender.getName())).findFirst();
+            if (playerName1.isPresent()) {
+                playerName = playerName1.get();
+            } else {
+                return;
+            }
+        } else {
+            playerName = args[0];
+        }
+        EntityPlayerMP player = getPlayer(server, sender, playerName);
+        EntityPlayerMP sender1 = getCommandSenderAsPlayer(sender);
+        SimpleTeamCommandsUtils.playerToPlayer(sender1, player);
+    }
 
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        if (args.length == 1)
+        {
+            return getListOfStringsMatchingLastWord(args, server.getOnlinePlayerNames());
+        } else {
+            return super.getTabCompletions(server, sender, args, targetPos);
+        }
+
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
     }
 }
